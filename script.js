9e896585481583e1aca8aaf086e1dd0a7db9be64
tushareshukla <123384228+tushareshukla@users.noreply.github.com>
2024-03-03 03:49:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // complete this js code
-function Person(name, age) {}
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-function Employee(name, age, jobTitle) {}
+    // Method to greet
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
+}
+
+// Define the Employee class inheriting from Person
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age); // Call the constructor of the parent class
+        this.jobTitle = jobTitle;
+    }
+
+    // Method to greet with job title
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
+}
 
 // Do not change code below this line
 window.Person = Person;
